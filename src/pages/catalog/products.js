93b38cd88d7daac707217ab097fc4b2347bae0b0
Larissa Vieira <larissavieira.ti@gmail.com>
2022-10-27 17:32:06 -0300

    Implementação do projeto em react Keller Bolsas

---
+++ src/pages/catalog/products.js
@@ -0,0 +1,58 @@
+const products = {
+  33: {
+    name: "Bolsa Silvia",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/houseofcaju/media/uploads/produtos/foto/b5a13495c5683correcao1204_0000s_0006s_0003_126.jpg",      
+    ],
+    price: 299.99,
+    promo_price: 199.99,
+    categories: [1, 2],
+  },
+  1: {
+    name: "Bolsa Vitória",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/lojamzq/media/uploads/produtos/foto/bb49c3eea6403bolsa-sacola-vermelha-feminina-em-couro-amora-20.jpg",
+    ],
+    price: 299.99,
+    promo_price: 199.99,
+    percent: 15,
+    categories: [1, 2],
+  },
+  4: {
+    name: "Bolsa Luísa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://images.tcdn.com.br/img/img_prod/639394/bolsa_feminina_transversal_classica_tiracolo_lancamento_2019_17_1_20201213205941.jpg",      
+    ],
+    price: 299.99,
+    promo_price: 199.99,
+    categories: [1, 2],
+  },
+  2: {
+    name: "Bolsa Maria",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/houseofcaju/media/uploads/produtos/foto/b4ed0fcc9ad04correcao1204_0000s_0001s_0003_054.jpg",      
+    ],
+    price: 299.99,
+    promo_price: 199.99,
+    percent: 15,
+    categories: [1, 2],
+  },
+};
+
+const categories = {
+  1: { name: "Bolsas" },
+  2: { name: "Acessórios" },
+};
+
+module.exports = {
+  products,
+  categories,
+};
